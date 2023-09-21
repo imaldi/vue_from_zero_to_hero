47db2817f1c92b0b52713d51a7ca319fcda2b610
aldi <aldiirsanmajid@gmail.com>
2023-09-22 05:49:26 +0700

    Creating an Application

--- src/main.js
+++ src/main.js
@@ -1,6 +1,37 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+// import the root component App from a single-file component.
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+// App Configuration Example
+// - App Level Error Handling
+app.config.errorHandler = (err) => {
+  /* handle error */
+}
+
+// - Registering component (only example)
+// This makes the TodoDeleteButton available for use anywhere in our app.
+// app.component('TodoDeleteButton', TodoDeleteButton)
+
+// An application instance won't render anything until its .mount() method is called. 
+// It expects a "container" argument, which can either be an actual DOM element or a selector string
+// Here the #app selector is automatically set to be from index.html when creating the project with npm
+app.mount('#app')
+
+Multiple application instances
+​
+
+// You are not limited to a single application instance on the same page. 
+// The createApp API allows multiple Vue applications to co-exist on the same page, 
+// each with its own scope for configuration and global assets
+// const app1 = createApp({
+//     /* ... */
+//   })
+//   app1.mount('#container-1')
+  
+//   const app2 = createApp({
+//     /* ... */
+//   })
+//   app2.mount('#container-2')
