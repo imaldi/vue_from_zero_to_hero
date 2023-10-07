25f889290a6007f700135c9b2e3ef9e45a804a48
aldi <aldiirsanmajid@gmail.com>
2023-10-07 22:55:16 +0700

    computed property basic

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <script setup>
 import { ref } from 'vue'
 
-// give each todo a unique id
 let id = 0
 
 const newTodo = ref('')
+const hideCompleted = ref(false)
 const todos = ref([
-  { id: id++, text: 'Learn HTML' },
-  { id: id++, text: 'Learn JavaScript' },
-  { id: id++, text: 'Learn Vue' }
+  { id: id++, text: 'Learn HTML', done: true },
+  { id: id++, text: 'Learn JavaScript', done: true },
+  { id: id++, text: 'Learn Vue', done: false }
 ])
 
 function addTodo() {
-  todos.value.push({ id: id++, text: newTodo.value })
+  todos.value.push({ id: id++, text: newTodo.value, done: false })
   newTodo.value = ''
 }
 
@@ -24,12 +24,22 @@ function removeTodo(todo) {
 <template>
   <form @submit.prevent="addTodo">
     <input v-model="newTodo">
-    <button>Add Todo</button>    
+    <button>Add Todo</button>
   </form>
   <ul>
     <li v-for="todo in todos" :key="todo.id">
-      {{ todo.text }}
+      <input type="checkbox" v-model="todo.done">
+      <span :class="{ done: todo.done }">{{ todo.text }}</span>
       <button @click="removeTodo(todo)">X</button>
     </li>
   </ul>
+  <button @click="hideCompleted = !hideCompleted">
+    {{ hideCompleted ? 'Show all' : 'Hide completed' }}
+  </button>
 </template>
+
+<style>
+.done {
+  text-decoration: line-through;
+}
+</style>
